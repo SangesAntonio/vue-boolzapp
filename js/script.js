305292a53380b4956a4bef813ece070ca1243e7a
SangesAntonio <sanges65@gmail.com>
2022-01-31 14:47:01 +0100

    added contact

--- js/script.js
+++ js/script.js
@@ -5,6 +5,28 @@ console.log('JS OK', Vue);
 const app = new Vue({
     el:'#root',
     data:{
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+          },
+          contacts: [
+            {
+              name: 'Michele',
+              avatar: '_1',
+            },
+            {
+              name: 'Fabio',
+              avatar: '_2',
+            },
+            {
+              name: 'Samuele',
+              avatar: '_3',
+            },
+            {
+              name: 'Luisa',
+              avatar: '_4',
+            },
+          ]
         
     },
     methods:{
